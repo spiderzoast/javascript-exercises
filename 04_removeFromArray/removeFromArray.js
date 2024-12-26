98e6342c4e58ee53b8b369a709979706c1b82424
spiderzoast <161398574+spiderzoast@users.noreply.github.com>
2024-12-26 00:50:39 -0300

    Create removeFromArray function
    It manages to solve test 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...toRemove) {
+  const newArray = [];
+  firstLoop: for (let i = 0; i < myArray.length; i++) {
+    if (Array.isArray(toRemove)) {
+      for (const item of toRemove){
+        if (myArray[i] === item) {
+          continue firstLoop;
+        }
+      }
+    } else if (myArray[i] === toRemove) {
+      continue;
+    }
+    newArray.push(myArray[i]);
+  }
+  return newArray;
 };
 
 // Do not edit below this line
