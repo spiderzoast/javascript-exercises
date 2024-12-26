b002ff07ac9ff1a00afb36c807e61fb7cf09db37
spiderzoast <161398574+spiderzoast@users.noreply.github.com>
2024-12-25 23:49:13 -0300

    Create reverseString function that solves test 1

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(stringToBeReversed) {
+  const myArray = stringToBeReversed.split("");
+  let reversedString = "";
+  for (let i = myArray.length - 1; i >= 0; i--) {
+    reversedString += myArray[i];
+  }
+  return reversedString;
 };
 
 // Do not edit below this line
