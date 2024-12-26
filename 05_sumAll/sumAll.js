79b12848a7182cf3c89bd3550ea5c1493086a36d
spiderzoast <161398574+spiderzoast@users.noreply.github.com>
2024-12-26 02:02:01 -0300

    Rework logic for sumAll function for test 3

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,14 +1,14 @@
-const sumAll = function(firstNumber, lastNumber) {
+const sumAll = function(num1, num2) {
   let total = 0;
   if (
-    !Number.isInteger(firstNumber) ||
-    !Number.isInteger(lastNumber)  ||
-    firstNumber < 0                ||
-    lastNumber < 0
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2) ||
+    num1 < 0                ||
+    num2 < 0
   ) {
     return "ERROR";
   }
-  for (let i = firstNumber; i <= lastNumber; i++) {
+  for (let i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
     total += i;
   }
   return total;
