4a805fccdefad0275881373c169c7d125c157124
spiderzoast <161398574+spiderzoast@users.noreply.github.com>
2024-12-26 04:06:22 -0300

    Create convertToCelsius and convertToFahrenheit functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return Number(((fahrenheit - 32) / 1.8).toFixed(2));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return Number(((celsius * 1.8) + 32).toFixed(2));
 };
 
 // Do not edit below this line
