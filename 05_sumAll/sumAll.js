c135ab9a7d757839c0d3cbe4afb5fdb7484dea82
spiderzoast <161398574+spiderzoast@users.noreply.github.com>
2024-12-26 01:56:28 -0300

    Create sumAll function
    Test 1 worked fine.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, lastNumber) {
+  let total = 0;
+  if (
+    !Number.isInteger(firstNumber) ||
+    !Number.isInteger(lastNumber)  ||
+    firstNumber < 0                ||
+    lastNumber < 0
+  ) {
+    return "ERROR";
+  }
+  for (let i = firstNumber; i <= lastNumber; i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
