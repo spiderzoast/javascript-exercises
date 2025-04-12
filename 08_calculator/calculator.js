9c02114cd8962bc901fd76e2c2d7aa3085128903
spiderzoast <161398574+spiderzoast@users.noreply.github.com>
2025-04-12 04:09:10 -0300

    Solve all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, item) => total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, item) => total * item, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (!num) {
+    return 1;
+  }
+	for (let i = num - 1; i > 0; i--) {
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
