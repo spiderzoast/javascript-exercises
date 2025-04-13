3eee101c6b087277782ccd5b3af19698e5e9da23
spiderzoast <161398574+spiderzoast@users.noreply.github.com>
2025-04-12 23:39:33 -0300

    Solve every test in 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  return arr.reduce((oldest, person) => {
+    if (!('name' in oldest)) {
+      return person;
+    }
 
+    const currentYear = new Date().getFullYear();
+
+    const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+    const personAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+
+    return oldestAge > personAge ? oldest : person;
+
+  }, {});
 };
 
 // Do not edit below this line
