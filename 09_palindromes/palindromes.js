2efb54c9121c00899cd680bb5bde721d16453141
spiderzoast <161398574+spiderzoast@users.noreply.github.com>
2025-04-12 22:18:41 -0300

    Solve every test in 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+  const cleanString = String(input).toLowerCase().replaceAll(/[^a-z0-9]/g, "")
+  const reversedString = cleanString.split("").reverse().join("");
+  return (cleanString === reversedString) ? true : false;
 };
 
 // Do not edit below this line
