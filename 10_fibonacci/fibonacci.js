e1408917ac79adfeaa76334010ddb9a611e10c9a
spiderzoast <161398574+spiderzoast@users.noreply.github.com>
2025-04-12 22:49:01 -0300

    Solve every test in 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+  const n = Number(input);
+  if (n === 0) {
+    return 0;
+  }
 
+  if (n < 0) {
+    return "OOPS";
+  }
+
+  if (n === 1 || n === 2) {
+    return 1;
+  }
+
+  let a = 1, b = 1;
+  
+  for (let i = 3; i <= n; i++) {
+      let temp = a + b;
+      a = b;
+      b = temp;
+  }
+  
+  return b;
 };
 
 // Do not edit below this line
