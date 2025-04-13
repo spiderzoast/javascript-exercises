2d1aba2105ba3cbb4a71fbd150aff8abd1c4b0fc
spiderzoast <161398574+spiderzoast@users.noreply.github.com>
2025-04-12 23:48:50 -0300

    Optimize code for 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,17 +1,10 @@
-const findTheOldest = function(arr) {
-  return arr.reduce((oldest, person) => {
-    if (!('name' in oldest)) {
-      return person;
-    }
-
+const findTheOldest = function(array) {
+  return array.reduce((oldest, person) => {
     const currentYear = new Date().getFullYear();
-
     const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
     const personAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
-
     return oldestAge > personAge ? oldest : person;
-
-  }, {});
+  });
 };
 
 // Do not edit below this line
